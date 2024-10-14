56f05fdd39c3489642b1da1bc6ee4fa999b632eb
brianisgoated <brianlee@Brians-MacBook-Pro.local>
2024-10-14 11:54:02 +0800

    #today i started on my MVP for models.py, i would like to finish the rest in my own time. #in the next lesson I will be able to start my MVP on views.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 # models.py
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+
+class Drill(Model):
+    drill_name = StringField()
+    instructions = StringField()
