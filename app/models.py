ece963aa275fa28c60f06ea54395ff9620d2d46f
brianisgoated <brianlee@Brians-MacBook-Pro.local>
2024-10-18 09:09:46 +0800

    #today i worked on my views.py and basically finished my MVPs unless i missed something, #next lesson I will try to finish the rest.

--- app/models.py
+++ app/models.py
@@ -6,4 +6,29 @@ class Drill(Model):
     drill_name = StringField()
     instructions = StringField()
     comments = StringField()
+    level = IntegerField()
+    tip = StringField()
+    likes = StringField
+
+    def json_response(self):
+        return {
+            'comments': self.comments, 
+            'drill_name': self.drill_name,
+            'instructions': self.instructions,
+            'level': self.level,
+            'tip': self.tip
+        }
+    
+    def change_tip(self, new_tip):
+        self.tip = new_tip
+        self.save()
+
+    def change_comments(self, new_comments):
+        self.comments = new_comments
+        self.save()
+
+    def add_likes(self):
+        self.likes += 1
+        self.save()
+
     
--- app/views.py
+++ app/views.py
@@ -1,5 +1,24 @@
 # views.py
 
 from banjo.urls import route_get, route_post
+from .models import Drill
 from settings import BASE_URL
 
+@route_post(BASE_URL + 'change_comments', args={'id': int, 'new_comments': str})
+def change_comments(args):
+    if Drill.objects.filter(id=args['id']).exists():
+        change_comments = Drill.objects.get(id=args['id'])
+        change_comments.change_comments(args['new_comments'])
+        return {'Fortune': change_comments.json_response()}
+    else:
+        return {'error' : 'no drill exists'}
+    
+    
+@route_post(BASE_URL + 'change_tip', args={'id': int, 'new_tip': str})
+def change_tip(args):
+    if Drill.objects.filter(id=args['id']).exists():
+        change_tip = Drill.objects.get(id=args['id'])
+        change_tip.change_tip(args['new_comments'])
+        return {'Fortune': change_tip.json_response()}
+    else:
+        return {'error' : 'no drill exists'}
